3b859ecfce415ec5570953f3984ea2a49e71906d
francoperez03 <franco.perez03@gmail.com>
2024-08-12 15:43:51 -0300

    chore: moved pino-pretty to dependencies

--- project/logger.ts
+++ project/logger.ts
@@ -1,15 +1,7 @@
 import pino from 'pino';
 
 const logger = pino({
-  level: process.env.LOG_LEVEL || 'info', // Puedes controlar el nivel de logs con una variable de entorno
-  transport: process.env.NODE_ENV !== 'production' ? {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: true,
-      ignore: 'pid,hostname',
-    },
-  } : undefined,
+  level: process.env.LOG_LEVEL || 'info',
 });
 
 export default logger;
